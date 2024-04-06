d22c1e2e4830601d4f86d8668794545721ce9f97
Villeman <villeman.arm@gmail.com>
2024-04-06 17:11:53 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Calendar from './components/Calendar.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <Calendar msg="You did it!" />
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+  .app-container
+    width: 100vw
+    min-height: 100vh
+    display: flex
+    justify-content: center
+    align-items: center
+
+</style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { reactive, ref, computed, onMounted, onUpdated, watch } from 'vue'
+
+defineProps<{
+	msg: string;
+}>();
+
+</script>
+
+<template>
+
+	<div class="calendar">
+		<div class="calendar__month">
+			<div class="calendar__month-prev-btn"></div>
+			<div class="calendar__month-current">April 2024</div>			
+			<div class="calendar__month-next-btn"></div>
+		</div>
+		<div class="calendar__week-days">
+			<div >Sun</div>
+			<div >Mon</div>
+			<div >Tue</div>
+			<div >Wed</div>
+			<div >Thu</div>
+			<div >Fri</div>
+			<div >Sat</div>
+		</div>
+		<div class="calendar__days"></div>
+	</div>
+
+</template>
+
+<style scoped lang="sass">
+	@import '@/assets/styles/constants.sass'
+
+	.calendar
+		width: 280px
+
+		font-size: 18px
+
+		background-color: $block-background-color
+
+	.calendar__month
+		display: flex
+		justify-content: space-between
+		align-items: center
+		padding: 8px
+
+	.calendar__month-prev-btn, .calendar__month-next-btn
+		width: 0
+		height: 0
+
+		cursor: pointer
+
+	.calendar__month-prev-btn
+		border-top: 6px solid transparent
+		border-bottom: 6px solid transparent
+		border-right: 12px solid #000
+
+
+	.calendar__month-next-btn
+		border-top: 6px solid transparent
+		border-bottom: 6px solid transparent
+		border-left: 12px solid #000
+
+	.calendar__week-days
+		display: grid
+		grid-template-columns: repeat(7, 1fr)
+		justify-items: center
+		padding-bottom: 8px
+
+		font-size: 14px
+
+</style>
+
+
+
+
+
