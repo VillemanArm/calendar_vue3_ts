fa068ce17d8314cfeea349e22b6dc8db54e8d027
Villeman <villeman.arm@gmail.com>
2024-04-08 11:51:58 +0300

    feat: add current day selection

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -8,6 +8,15 @@ defineProps<{
 
 const store = useCalendarStore()
 
+function handleDayClick(event: MouseEvent) {
+	const target = event.target as HTMLElement;
+	if (target.matches('.calendar__day') && target.innerText) {
+			console.log(target.innerText);
+
+				
+	}
+}
+
 </script>
 
 <template>
@@ -27,7 +36,20 @@ const store = useCalendarStore()
 			<div >Fri</div>
 			<div >Sat</div>
 		</div>
-		<div class="calendar__days"></div>
+		<div class="calendar__days" @click="(event: MouseEvent) => {handleDayClick(event)}">
+			<div 
+				v-for="day, index in store.displayedDays" 
+				key="index" 
+				:class="{
+					'calendar__day': true, 
+					'calendar__current-day': (store.currentYear === store.selectedYear && store.currentMonth === store.selectedMonth && store.currentDate === day)
+
+
+				}"
+			>
+				{{ day }}
+			</div>
+		</div>
 	</div>
 
 </template>
@@ -36,7 +58,7 @@ const store = useCalendarStore()
 	@import '@/assets/styles/constants.sass'
 
 	.calendar
-		width: 280px
+		width: 245px
 
 		font-size: 18px
 
@@ -72,6 +94,26 @@ const store = useCalendarStore()
 		padding-bottom: 8px
 
 		font-size: 14px
+
+	.calendar__days
+		display: grid
+		grid-template-columns: repeat(7, 1fr)
+		justify-items: center
+
+	.calendar__day
+		width: 35px
+		height: 35px
+		display: grid
+		align-items: center
+		justify-items: center
+
+		line-height: 100%
+
+		cursor: pointer
+
+	.calendar__current-day
+		border: 1px solid $light-blue
+
 
 </style>
 
--- src/stores/Calendar.ts
+++ src/stores/Calendar.ts
@@ -4,6 +4,10 @@ import { defineStore } from 'pinia';
 export const useCalendarStore = defineStore('calendar', () => {
   const dayInMs = 86400000;
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const now = new Date
+  const currentDate = now.getDate()
+  const currentMonth = now.getMonth()
+  const currentYear = now.getFullYear()
 
   let dateInMs = ref(Date.now());
   let selectedYear = computed(() => new Date(dateInMs.value).getFullYear());
@@ -12,18 +16,21 @@ export const useCalendarStore = defineStore('calendar', () => {
   let daysInMonth = computed(() => new Date(selectedYear.value, selectedMonth.value + 1, 0).getDate())
   let displayedDays = computed(() => {
     const firstDay = new Date(selectedYear.value, selectedMonth.value, 1).getDay()
-    const lastDay = new Date(selectedYear.value, selectedMonth.value, daysInMonth.value).getDay()
 
     const days: Array< '' | number> = Array(firstDay - 1).fill('')
+
     for (let i:number = 1; i <= daysInMonth.value; i++) {
       days.push(i)
     }
-    while (days.length % 7 !== 0) {
-      days.push('')
-    }
+    
+    // while (days.length % 7 !== 0) {
+    //   days.push('')
+    // }
 
     return days
   })
+
+
 
   function changeSelectedMonth(monthAmount: number) {
     dateInMs.value += monthAmount * dayInMs * daysInMonth.value;
@@ -37,5 +44,5 @@ export const useCalendarStore = defineStore('calendar', () => {
   //   selectedMonth.value = newDate.getMonth();
   // }, { deep: true });
 
-  return { selectedYear, selectedMonthName, displayedDays, changeSelectedMonth };
+  return { selectedYear, selectedMonth, selectedMonthName, displayedDays, currentDate, currentMonth, currentYear, changeSelectedMonth };
 });
